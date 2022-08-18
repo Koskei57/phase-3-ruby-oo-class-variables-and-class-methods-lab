6c3dee4b52e13b036f2f95cbbb669a94647d128b
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-08-19 00:19:21 +0300

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -25,4 +25,26 @@ class Song
     @@artists.uniq
   end
 
+  def self.genre_count
+    genre_count = {}
+    @@genres.map do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+  def self.artist_count
+    artist_count = {}
+    @@artists.map do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 end
